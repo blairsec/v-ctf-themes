b05b1e03ac5b97e172fba5f1abfb1ee406890f6d
Kevin Higgs <github@kevinhiggs.com>
2018-11-22 15:22:48 -0500

    2017 and 2018

---
+++ 2017/Background.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg"></div>
+</template>
+
+<style lang="scss">
+@import './colors.scss';
+
+div.bg {
+  width: 100vw;
+  height: 100vh;
+  z-index: -1000000;
+  position: fixed;
+  background-image: url('./background_blur.jpg');
+  background-size: cover;
+  filter: brightness(40%);
+  background-size: cover;
+}
+</style>
