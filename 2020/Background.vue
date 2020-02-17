2af01c36b4bf97f8cc9a6d2afd67ce4a1f3c9d6e
Kevin Higgs <github@kevinhiggs.com>
2020-02-17 14:00:23 -0500

    style updates

---
+++ 2020/Background.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg"></div>
+</template>
+
+<style lang="scss">
+@import './colors.scss';
+
+body {
+  background-color: black;
+}
+
+div.bg {
+  width: 100vw;
+  height: 100vh;
+  z-index: -1000000;
+  position: fixed;
+  background: url(bg.jpg);
+  opacity: 0.45;
+  background-size: cover;
+  background-position: center center;
+  position: fixed;
+  height: 100%;
+  width: 100%;
+}
+</style>
